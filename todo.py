7ae51242d542fc568e751836f5ec26f1450047d7
chaitanya <gopi.daiict@gmail.com>
2010-04-12 11:19:52 +0530

    added error check for null task; and basic documentation

--- todo.py
+++ todo.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
-
 import sys, os
+
+""" 
+this a simple script to help me manage my todo's.
+the current functionalities include,
+--add a new task.
+
+--error checking [ comming ]
+--delete task
+--update task
+
+--sorted views
+--tagging; etc.
+
+"""
 
 todoFile = os.environ['PROJECTS'] + "/todo.txt"
 
 def addTask(task):
 	todotxt = open(todoFile, 'a')
-	todotxt.write(task)
+	todotxt.write(task + "\n")
 	todotxt.close()
 
 	print 'task added'
@@ -14,4 +27,5 @@ def addTask(task):
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
 		print 'usage: todo [ new-task-to-be-added. ]'
+		sys.exit(-1)
 	addTask(sys.argv[1])
